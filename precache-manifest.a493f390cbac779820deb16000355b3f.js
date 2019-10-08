94010a1777858015c00467912f252290f73e42d2
opticalfalicy <opticalfalicy@gmail.com>
2019-10-08 10:40:34 -0400

    Updates

---
+++ precache-manifest.a493f390cbac779820deb16000355b3f.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d0c5eef256382e4bbf364046bbbf1a97",
+    "url": "/shuffolio/index.html"
+  },
+  {
+    "revision": "eac3ec20b70307d1eabd",
+    "url": "/shuffolio/static/css/main.e8a35b55.chunk.css"
+  },
+  {
+    "revision": "b625a4e40141ebeabe84",
+    "url": "/shuffolio/static/js/2.ddce5800.chunk.js"
+  },
+  {
+    "revision": "eac3ec20b70307d1eabd",
+    "url": "/shuffolio/static/js/main.688ceda1.chunk.js"
+  },
+  {
+    "revision": "cdf130c437775de9c86b",
+    "url": "/shuffolio/static/js/runtime-main.9b18be50.js"
+  },
+  {
+    "revision": "70be7fbc96c84bfa6f4ad244675eefe2",
+    "url": "/shuffolio/static/media/bus.70be7fbc.JPG"
+  },
+  {
+    "revision": "25bf045ca257e971124f3997d89f321c",
+    "url": "/shuffolio/static/media/logo.25bf045c.svg"
+  }
+]);
