fb4961f5a5690bb0c3f8e8479769da8f2231fc3b
opticalfalicy <opticalfalicy@gmail.com>
2019-10-09 12:21:50 -0400

    Image carousel

--- shuff/src/pages/Home.js
+++ shuff/src/pages/Home.js
@@ -1,6 +1,13 @@
 import React from 'react';
 import {PgRight, PgLeft, More} from '../components'
+import { Carousel } from 'react-responsive-carousel';
+import "react-responsive-carousel/lib/styles/carousel.min.css";
 import '../sass/pages/Home.sass'
+
+import bus from '../media/bus.JPG';
+import rund from '../media/rund.jpg';
+import sky from '../media/sky.jpg';
+
 
 export default class Home extends React.Component {
     constructor(){
@@ -10,11 +17,23 @@ export default class Home extends React.Component {
     render (){
         return(
             <div className="home-div">
-                <div className="home-slide">
-                <PgLeft />
-                <More />
-                <PgRight />
-                </div>
+             {/* <div className="home-slide">  */}
+            <Carousel className="home-carousel" showThumbs={false}>
+                    <div className="carousel-item">
+                        <img className="carousel-img" src={bus}></img>
+                        <More className="more-button" />
+                    </div>  
+                    <div className="carousel-item">
+                        <img className="carousel-img" src={sky}/>
+                        <More className="more-button" />
+                    </div>  
+                    <div className="carousel-item">
+                        <img className="carousel-img" src={rund}/>
+                        <More className="more-button" />
+                    </div>  
+                    </Carousel>
+                {/* // <More></More> */}
+                {/* </div> */}
                 
 
     </div>
